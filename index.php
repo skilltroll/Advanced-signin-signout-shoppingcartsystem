d3fda5c74eef29b8eabe985c05b9a623f5e00b5a
Ali omar <47419773+skilltroll@users.noreply.github.com>
2022-08-09 20:09:37 +0300

    Update index.php

--- index.php
+++ index.php
@@ -31,25 +31,6 @@ if(isset($_POST['add_to_cart'])){
    }
 
 };
-/* Moved to cart.php DELETE THIS
-if(isset($_POST['update_cart'])){
-   $update_quantity = $_POST['cart_quantity'];
-   $update_id = $_POST['cart_id'];
-   mysqli_query($conn, "UPDATE `cart` SET quantity = '$update_quantity' WHERE id = '$update_id'") or die('query failed');
-   $message[] = 'cart quantity updated successfully!';
-}
-
-if(isset($_GET['remove'])){
-   $remove_id = $_GET['remove'];
-   mysqli_query($conn, "DELETE FROM `cart` WHERE id = '$remove_id'") or die('query failed');
-   header('location:index.php');
-}
-  
-if(isset($_GET['delete_all'])){
-   mysqli_query($conn, "DELETE FROM `cart` WHERE user_id = '$user_id'") or die('query failed');
-   header('location:index.php');
-}
-*/
 ?>
 
 <!DOCTYPE html>
@@ -126,62 +107,6 @@ if(isset($message)){
 
    </div>
 
-</div>
-<!--all of the below elemets can be deleted cus its all moved to cart.php no need for this in index file-->
-<div class="shopping-cart" style="display:none;">
-   <h1 class="heading">shopping cart</h1>
-
-   <table>
-      <thead>
-         <th>image</th>
-         <th>name</th>
-         <th>price</th>
-         <th>quantity</th>
-         <th>total price</th>
-         <th>action</th>
-      </thead>
-      <tbody>
-      <?php
-         $cart_query = mysqli_query($conn, "SELECT * FROM `cart` WHERE user_id = '$user_id'") or die('query failed');
-         $grand_total = 0;
-         if(mysqli_num_rows($cart_query) > 0){
-            while($fetch_cart = mysqli_fetch_assoc($cart_query)){
-      ?>
-         <tr>
-            <td><img src="images/<?php echo $fetch_cart['image']; ?>" height="100" alt=""></td>
-            <td><?php echo $fetch_cart['name']; ?></td>
-            <td>$<?php echo $fetch_cart['price']; ?>/-</td>
-            <td>
-               <form action="" method="post">
-                  <input type="hidden" name="cart_id" value="<?php echo $fetch_cart['id']; ?>">
-                  <input type="number" min="1" name="cart_quantity" value="<?php echo $fetch_cart['quantity']; ?>">
-                  <input type="submit" name="update_cart" value="update" class="option-btn">
-               </form>
-            </td>
-            <td>$<?php echo $sub_total = ($fetch_cart['price'] * $fetch_cart['quantity']); ?>/-</td>
-            <td><a href="index.php?remove=<?php echo $fetch_cart['id']; ?>" class="delete-btn" onclick="return confirm('remove item from cart?');">remove</a></td>
-         </tr>
-      <?php
-         $grand_total += $sub_total;
-            }
-         }else{
-            echo '<tr><td style="padding:20px; text-transform:capitalize;" colspan="6">no item added</td></tr>';
-         }
-      ?>
-      <tr class="table-bottom">
-         <td colspan="4">grand total :</td>
-         <td>$<?php echo $grand_total; ?>/-</td>
-         <td><a href="index.php?delete_all" onclick="return confirm('delete all from cart?');" class="delete-btn <?php echo ($grand_total > 1)?'':'disabled'; ?>">delete all</a></td>
-      </tr>
-   </tbody>
-   </table>
-
-   <div class="cart-btn">  
-      <a href="#" class="btn <?php echo ($grand_total > 1)?'':'disabled'; ?>">proceed to checkout</a>
-   </div>
-
-</div>
-</div>
-      
+</div>      
 </body>
 </html>
